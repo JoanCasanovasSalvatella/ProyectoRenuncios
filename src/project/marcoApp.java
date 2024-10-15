931a0f1a6bc9f54ce54265ddc280c9a00f451cf7
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2024-10-15 16:33:18 +0200

    Add files via upload

---
+++ src/project/marcoApp.java
@@ -0,0 +1,20 @@
+package src.project;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class marcoApp extends JFrame {
+	 public marcoApp() {
+	        setTitle("Mi Ventana"); //Establecer el titulo de la ventana
+	        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // Obtener el tamaño de la pantalla
+	        setSize(screenSize.width, screenSize.height); // Establecer el tamaño de la ventana al tamaño de la pantalla
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }
+
+	    public static void main(String[] args) {
+	        marcoApp ventana = new marcoApp();
+	        ventana.setVisible(true);
+	    }
+}
