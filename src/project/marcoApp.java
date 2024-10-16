dac8ca6d109cd21edf69863c81795ec576449ef4
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2024-10-16 19:47:10 +0200

    Update marcoApp.java

--- src/project/marcoApp.java
+++ src/project/marcoApp.java
@@ -11,6 +11,9 @@ public class marcoApp extends JFrame {
 	        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // Obtener el tamaño de la pantalla
 	        setSize(screenSize.width, screenSize.height); // Establecer el tamaño de la ventana al tamaño de la pantalla
 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		landingPage landing = new landingPage();
+	        add(landing);
 	    }
 
 	    public static void main(String[] args) {
